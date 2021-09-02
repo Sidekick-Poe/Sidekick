e722168943627240e6971e07aab8a5fc9c744c8c
Yanic Lemire <lemire.yanic@gmail.com>
2021-09-02 01:04:32 -0400

    Updated Delve cheatsheet
    Related to #1

--- src/Sidekick.Modules.Cheatsheets/Delve/FossilModel.cs
+++ src/Sidekick.Modules.Cheatsheets/Delve/FossilModel.cs
@@ -2,15 +2,17 @@ namespace Sidekick.Modules.Cheatsheets.Delve
 {
     public class FossilModel
     {
-        public FossilModel(string name, RewardValue value, bool behindFracturedWall = false)
+        public FossilModel(string name, RewardValue value, bool behindFracturedWall = false, bool inFossilRoom = false)
         {
             Name = name;
             Value = value;
             BehindFracturedWall = behindFracturedWall;
+            InFossilRoom = inFossilRoom;
         }
 
         public string Name { get; set; }
         public RewardValue Value { get; set; }
         public bool BehindFracturedWall { get; set; }
+        public bool InFossilRoom { get; set; }
     }
 }
