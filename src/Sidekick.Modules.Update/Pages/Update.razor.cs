6f94cf617117bf6965d4ca86cd35c933b3248a46
Yanic Lemire <lemire.yanic@gmail.com>
2021-09-03 23:07:05 -0400

    Allow updater to fail and keep initializing the app

--- src/Sidekick.Modules.Update/Pages/Update.razor.cs
+++ src/Sidekick.Modules.Update/Pages/Update.razor.cs
@@ -42,7 +42,7 @@ namespace Sidekick.Modules.Update.Pages
                 StateHasChanged();
                 var release = await GitHubClient.GetLatestRelease();
 
-                if (!GitHubClient.IsUpdateAvailable(release) || Environment.IsDevelopment())
+                if (release == null || !GitHubClient.IsUpdateAvailable(release) || Environment.IsDevelopment())
                 {
                     await Task.Delay(750);
                     NavigationManager.NavigateTo("/setup");
@@ -71,7 +71,7 @@ namespace Sidekick.Modules.Update.Pages
             {
                 Logger.LogError(ex.Message);
                 await AppService.OpenNotification(UpdateResources.Failed);
-                AppService.Shutdown();
+                NavigationManager.NavigateTo("/setup");
             }
         }
 
