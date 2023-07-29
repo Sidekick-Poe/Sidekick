15b091202c149b14440843cf4adb027b4204dbcd
Yanic Lemire <lemire.yanic@gmail.com>
2023-07-29 14:54:35 -0400

    Removed MAUI project

--- src/Sidekick.Maui/InitialWindow.cs
+++
@@ -1,9 +0,0 @@
-namespace Sidekick.Maui;
-
-public class InitialWindow : BlazorWindow
-{
-    public InitialWindow(IServiceProvider serviceProvider, string initialUrl)
-        : base(serviceProvider, initialUrl)
-    {
-    }
-}
