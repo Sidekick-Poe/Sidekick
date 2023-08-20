f2efd2eb51b35680bcbc8debe5508a0d7290fa91
Yanic Lemire <lemire.yanic@gmail.com>
2023-08-20 03:11:37 -0400

    Fixed tray icon showing multiple times after a cache reset.

--- src/Sidekick/Electron/ElectronTrayProvider.cs
+++ src/Sidekick/Electron/ElectronTrayProvider.cs
@@ -9,19 +9,23 @@ namespace Sidekick.Electron
     {
         private readonly IWebHostEnvironment webHostEnvironment;
         private readonly IViewLocator viewLocator;
-        private readonly ILogger<ElectronTrayProvider> logger;
 
         public ElectronTrayProvider(IWebHostEnvironment webHostEnvironment,
-            IViewLocator viewLocator,
-            ILogger<ElectronTrayProvider> logger)
+            IViewLocator viewLocator)
         {
             this.webHostEnvironment = webHostEnvironment;
             this.viewLocator = viewLocator;
-            this.logger = logger;
         }
+
+        private bool Initialized { get; set; }
 
         public void Initialize(List<TrayMenuItem> items)
         {
+            if (Initialized)
+            {
+                return;
+            }
+
             ElectronNET.API.Electron.Tray.Show(Path.Combine(webHostEnvironment.ContentRootPath, "Assets/icon.png"), items.Select(x => new MenuItem()
             {
                 Label = x.Label,
@@ -30,6 +34,8 @@ namespace Sidekick.Electron
             }).ToArray());
             ElectronNET.API.Electron.Tray.OnDoubleClick += (_, _) => viewLocator.Open("/settings");
             ElectronNET.API.Electron.Tray.SetToolTip("Sidekick");
+
+            Initialized = true;
         }
     }
 }
