10771cd71fd0c8f6ad83caf023b2ca7abe5d27bc
Yanic Lemire <lemire.yanic@gmail.com>
2023-08-20 03:11:46 -0400

    Removed Theme from settings

--- src/Sidekick.Modules.Settings/SettingsModel.cs
+++ src/Sidekick.Modules.Settings/SettingsModel.cs
@@ -31,8 +31,6 @@ namespace Sidekick.Modules.Settings
 
         public Guid? CurrentKey { get; set; }
 
-        public string Theme { get; set; }
-
         public bool Trade_CloseWithMouse { get; set; }
 
         public bool Map_CloseWithMouse { get; set; }
