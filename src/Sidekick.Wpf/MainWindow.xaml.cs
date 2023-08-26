d4951bfb6b348696b0d23e807bc6187b0cdc4e0d
Yanic Lemire <lemire.yanic@gmail.com>
2023-08-26 00:36:52 -0400

    Remove white flicker when opening window

--- src/Sidekick.Wpf/MainWindow.xaml.cs
+++ src/Sidekick.Wpf/MainWindow.xaml.cs
@@ -1,4 +1,6 @@
 using System.Windows;
+using System.Windows.Media;
+using Microsoft.Extensions.DependencyInjection;
 
 namespace Sidekick.Wpf;
 
@@ -7,9 +9,37 @@ namespace Sidekick.Wpf;
 /// </summary>
 public partial class MainWindow : Window
 {
+    private IServiceScope Scope { get; set; }
+
     public MainWindow()
     {
-        Resources.Add("services", App.ServiceProvider);
+        Scope = App.ServiceProvider.CreateScope();
+
+        Resources.Add("services", Scope.ServiceProvider);
         InitializeComponent();
+
+        WebView.Loaded += WebView_Loaded;
+    }
+
+    private void WebView_Loaded(object sender, RoutedEventArgs e)
+    {
+        WebView.WebView.NavigationCompleted += WebView_NavigationCompleted;
+    }
+
+    private void WebView_NavigationCompleted(object? sender, Microsoft.Web.WebView2.Core.CoreWebView2NavigationCompletedEventArgs e)
+    {
+        // This avoids the white flicker which is caused by the page content not being loaded initially. We show the webview control only when the content is ready.
+        WebView.Visibility = Visibility.Visible;
+
+        // The window background is transparent to avoid any flickering when opening a window. When the webview content is ready we need to set a background color. Otherwise mouse clicks will go through the window.
+        Background = (Brush?)new BrushConverter().ConvertFrom("#000000");
+
+        InvalidateVisual();
+    }
+
+    protected override void OnClosed(EventArgs e)
+    {
+        base.OnClosed(e);
+        Scope.Dispose();
     }
 }
