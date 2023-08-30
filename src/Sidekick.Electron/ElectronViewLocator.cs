d61a14a2900f79779f0804025317662b79370b58
Yanic Lemire <lemire.yanic@gmail.com>
2023-08-30 14:36:50 -0400

    Fixed pipeline

--- src/Sidekick.Electron/ElectronViewLocator.cs
+++ src/Sidekick.Electron/ElectronViewLocator.cs
@@ -32,9 +32,6 @@ namespace Sidekick.Electron
             });
         }
 
-        /// <inheritdoc/>
-        public bool IsElectron => true;
-
         private List<BrowserWindow> Browsers { get; } = new();
 
         private List<SidekickView> Views { get; } = new();
