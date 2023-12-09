1e5cb770123ab0a229fe3fe57eaaabb94b5951fe
Yanic Lemire <lemire.yanic@gmail.com>
2023-12-09 15:48:33 -0500

    Removed metamorph tests

---
+++ tests/Sidekick.Apis.Poe.Tests/Parser/UltimatumParsing.cs
@@ -0,0 +1,37 @@
+using Sidekick.Common.Game.Items;
+using Xunit;
+
+namespace Sidekick.Apis.Poe.Tests.Parser
+{
+    [Collection(Collections.Mediator)]
+    public class UltimatumParsing
+    {
+        private readonly IItemParser parser;
+
+        public UltimatumParsing(ParserFixture fixture)
+        {
+            parser = fixture.Parser;
+        }
+
+        [Fact]
+        public void NoxiousCatalyst()
+        {
+            var actual = parser.ParseItem(@"Item Class: Stackable Currency
+Rarity: Currency
+Noxious Catalyst
+--------
+Stack Size: 1/10
+--------
+Adds quality that enhances Physical and Chaos Damage modifiers on a ring, amulet or belt
+Replaces other quality types
+--------
+Right click this item then left click a ring, amulet or belt to apply it. Has greater effect on lower-rarity jewellery. The maximum quality is 20%.
+");
+
+            Assert.Equal(Class.StackableCurrency, actual.Header.Class);
+            Assert.Equal(Rarity.Currency, actual.Metadata.Rarity);
+            Assert.Equal(Category.Currency, actual.Metadata.Category);
+            Assert.Equal("Noxious Catalyst", actual.Metadata.Type);
+        }
+    }
+}
