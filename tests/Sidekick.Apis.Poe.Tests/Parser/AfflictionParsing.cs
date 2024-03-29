e3920f791cadf564e007006a5609b7e135012cb7
Dominique Alexandre <d.alexandre@outlook.com>
2024-03-29 15:10:23 -0400

    Remove affliction test

--- tests/Sidekick.Apis.Poe.Tests/Parser/AfflictionParsing.cs
+++ tests/Sidekick.Apis.Poe.Tests/Parser/AfflictionParsing.cs
@@ -16,25 +16,25 @@ namespace Sidekick.Apis.Poe.Tests.Parser
         [Fact]
         public void BulbonicTrail()
         {
-            var actual = parser.ParseItem(@"Item Class: Charms
-Rarity: Magic
-Corvine Charm of the Trickster
---------
-Item Level: 70
---------
-Recover 2% of Life on Kill
-Recover 2% of Energy Shield on Kill
-Recover 2% of Mana on Kill
---------
-Place into an allocated Charm Socket on the Wildwood Primalist Passive Skill Tree. Right click to remove from the Socket.
---------
-Unmodifiable
-");
+//            var actual = parser.ParseItem(@"Item Class: Charms
+//Rarity: Magic
+//Corvine Charm of the Trickster
+//--------
+//Item Level: 70
+//--------
+//Recover 2% of Life on Kill
+//Recover 2% of Energy Shield on Kill
+//Recover 2% of Mana on Kill
+//--------
+//Place into an allocated Charm Socket on the Wildwood Primalist Passive Skill Tree. Right click to remove from the Socket.
+//--------
+//Unmodifiable
+//");
 
-            Assert.Equal(Class.AfflictionCharms, actual.Header.Class);
-            Assert.Equal(Category.Affliction, actual.Metadata.Category);
-            Assert.Equal(Rarity.Magic, actual.Metadata.Rarity);
-            Assert.Equal("Corvine Charm", actual.Metadata.Type);
+//            Assert.Equal(Class.AfflictionCharms, actual.Header.Class);
+//            Assert.Equal(Category.Affliction, actual.Metadata.Category);
+//            Assert.Equal(Rarity.Magic, actual.Metadata.Rarity);
+//            Assert.Equal("Corvine Charm", actual.Metadata.Type);
         }
     }
 }
