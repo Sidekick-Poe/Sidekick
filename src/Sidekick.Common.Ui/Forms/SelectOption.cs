fb4c6e0d9ec296d842fa9593c4d07c00b4615e8a
Yanic Lemire <lemire.yanic@gmail.com>
2024-07-07 21:14:31 -0400

    UI updates including various font adjustments

---
+++ src/Sidekick.Common.Ui/Forms/SelectOption.cs
@@ -0,0 +1,8 @@
+﻿namespace Sidekick.Common.Ui.Forms;
+
+public class SelectOption
+{
+    public required string Label { get; init; }
+
+    public required string Value { get; init; }
+}
--- src/Sidekick.Common.Ui/tailwind.config.js
+++ src/Sidekick.Common.Ui/tailwind.config.js
@@ -8,6 +8,15 @@ module.exports = {
         fontFamily: {
             'sans': ['fontin', ...defaultTheme.fontFamily.sans],
             'caps': ['fontin-smallcaps', 'fontin', ...defaultTheme.fontFamily.sans],
+        },
+        fontSize: {
+            xs: '0.625rem',
+            sm: '0.75rem',
+            base: '0.875rem',
+            lg: '1',
+            xl: '1.25rem',
+            '2xl': '1.5rem',
+            '3xl': '1.875rem'
         }
     }
 }
