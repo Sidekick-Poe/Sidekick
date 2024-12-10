149fb4d2f3933057db2e38d40707ea32dc9c476b
leMicin <lemire.yanic@gmail.com>
2024-12-10 13:09:52 -0500

    Fixed trade results after a PoE trade API change

--- src/Sidekick.Apis.Poe/Trade/Results/Magnitude.cs
+++ src/Sidekick.Apis.Poe/Trade/Results/Magnitude.cs
@@ -3,9 +3,5 @@ namespace Sidekick.Apis.Poe.Trade.Results
     public class Magnitude
     {
         public string? Hash { get; set; }
-
-        public double? Min { get; set; }
-
-        public double? Max { get; set; }
     }
 }
