debb56e72ab6c22ef66eab8ffed48a5c9ead0792
leMicin <lemire.yanic@gmail.com>
2024-12-15 13:15:47 -0500

    Formatting

--- src/Sidekick.Apis.Poe/Trade/Requests/Filters/SearchFilterValue.cs
+++ src/Sidekick.Apis.Poe/Trade/Requests/Filters/SearchFilterValue.cs
@@ -1,37 +1,40 @@
 using Sidekick.Apis.Poe.Trade.Models;
 
-namespace Sidekick.Apis.Poe.Trade.Requests.Filters
+namespace Sidekick.Apis.Poe.Trade.Requests.Filters;
+
+internal class SearchFilterValue
 {
-    internal class SearchFilterValue
+    internal SearchFilterValue()
     {
-        internal SearchFilterValue()
-        { }
-
-        public SearchFilterValue(PropertyFilter filter)
-        {
-            if (filter.ValueType == FilterValueType.Boolean)
-            {
-                Option = filter.Checked == true ? "true" : "false";
-            }
-            Min = filter.Min;
-            Max = filter.Max;
-        }
-
-        public SearchFilterValue(ModifierFilter filter)
-        {
-            Option = filter.Line.OptionValue;
-            Min = filter.Min;
-            Max = filter.Max;
-        }
-
-        public SearchFilterValue(PseudoModifierFilter filter)
-        {
-            Min = filter.Min;
-            Max = filter.Max;
-        }
-
-        public object? Option { get; set; }
-        public decimal? Min { get; set; }
-        public decimal? Max { get; set; }
     }
+
+    public SearchFilterValue(PropertyFilter filter)
+    {
+        if (filter.ValueType == FilterValueType.Boolean)
+        {
+            Option = filter.Checked == true ? "true" : "false";
+        }
+
+        Min = filter.Min;
+        Max = filter.Max;
+    }
+
+    public SearchFilterValue(ModifierFilter filter)
+    {
+        Option = filter.Line.OptionValue;
+        Min = filter.Min;
+        Max = filter.Max;
+    }
+
+    public SearchFilterValue(PseudoModifierFilter filter)
+    {
+        Min = filter.Min;
+        Max = filter.Max;
+    }
+
+    public object? Option { get; set; }
+
+    public decimal? Min { get; set; }
+
+    public decimal? Max { get; set; }
 }
