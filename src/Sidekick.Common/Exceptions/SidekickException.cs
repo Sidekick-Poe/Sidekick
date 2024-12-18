3ca458ed836efc4698e534ef21e4fd53e4a9e457
leMicin <lemire.yanic@gmail.com>
2024-12-18 17:29:49 -0500

    Added comment to exception

--- src/Sidekick.Common/Exceptions/SidekickException.cs
+++ src/Sidekick.Common/Exceptions/SidekickException.cs
@@ -20,5 +20,12 @@ public class SidekickException : Exception
 
     public string? AdditionalInformation { get; }
 
+    /// <summary>
+    /// Gets or sets a value indicating whether the application should exit in response to an exception. This changes the button on the error screen from 'Close' to 'Exit Application'.
+    /// </summary>
+    /// <remarks>
+    /// This property is typically used to signal that a critical error has occurred, requiring the application to shut down. It can be
+    /// particularly useful for error handling logic to guide application behavior during exceptional scenarios.
+    /// </remarks>
     public bool ExitApplication { get; set; }
 }
