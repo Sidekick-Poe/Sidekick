91e525fb997225b017355115bc597626d06a2fc7
Dominique Alexandre <d.alexandre@outlook.com>
2024-12-20 11:15:34 -0500

    Order language choices in the settings

--- src/Sidekick.Common/Exceptions/ApiErrorException.cs
+++ src/Sidekick.Common/Exceptions/ApiErrorException.cs
@@ -8,7 +8,7 @@ public class ApiErrorException : SidekickException
     }
 
     public ApiErrorException(string? additionalInformation)
-        : base("An error occured while trying to get the results from the official trade API. The official trade website may be down. Please try again later or open a ticket on github.", additionalInformation)
+        : base("An error occured while trying to get the results from the official trade API. The official trade website may be down. Please try again later or open a ticket on github.", additionalInformation ?? string.Empty)
     {
     }
 }
