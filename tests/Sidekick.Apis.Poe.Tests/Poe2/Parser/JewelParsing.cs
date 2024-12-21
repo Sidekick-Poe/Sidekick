b41b3657e92936afb06b2e6b0e185a6906346307
leMicin <lemire.yanic@gmail.com>
2024-12-20 22:38:21 -0500

    Added time lost jewel test

--- tests/Sidekick.Apis.Poe.Tests/Poe2/Parser/JewelParsing.cs
+++ tests/Sidekick.Apis.Poe.Tests/Poe2/Parser/JewelParsing.cs
@@ -37,4 +37,30 @@ Place into an allocated Jewel Socket on the Passive Skill Tree. Right click to r
         actual.AssertHasModifier(ModifierCategory.Explicit, "#% increased Magnitude of Ailments you inflict", 7);
         actual.AssertHasModifier(ModifierCategory.Explicit, "#% increased Pin Buildup", 14);
     }
+
+    [Fact]
+    public void ParseTimeLost()
+    {
+        var actual = parser.ParseItem(
+            @"Item Class: Jewels
+Rarity: Rare
+Fulgent Shard
+Time-Lost Ruby
+--------
+Radius: Small
+--------
+Item Level: 64
+--------
+Small Passive Skills in Radius also grant 2% increased Totem Damage
+Notable Passive Skills in Radius also grant 4% increased Magnitude of Bleeding you inflict
+Notable Passive Skills in Radius also grant 7% increased Stun Buildup with Maces
+--------
+Place into an allocated Jewel Socket on the Passive Skill Tree. Right click to remove from the Socket.");
+
+        Assert.Equal(Category.Jewel, actual.Metadata.Category);
+        Assert.Equal(Rarity.Rare, actual.Metadata.Rarity);
+        Assert.Equal("Time-Lost Ruby", actual.Metadata.Type);
+        Assert.Null(actual.Metadata.Name);
+        Assert.Equal(64, actual.Properties.ItemLevel);
+    }
 }
