91ba1f81de023cd06ba37c504817aa2d1c26fe7d
Dominique Alexandre <d.alexandre@outlook.com>
2024-12-29 16:36:28 -0500

    Create project using web template

---
+++ src/Sidekick.Linux/ExceptionHandlingMiddleware.cs
@@ -0,0 +1,39 @@
+using Sidekick.Common.Exceptions;
+using Sidekick.Common.Ui.Views;
+
+namespace Sidekick.Linux;
+
+public class ExceptionHandlingMiddleware
+{
+    private readonly ILogger<ExceptionHandlingMiddleware> logger;
+    private readonly RequestDelegate requestDelegate;
+    private readonly IViewLocator viewLocator;
+
+    public ExceptionHandlingMiddleware(
+        ILogger<ExceptionHandlingMiddleware> logger,
+        RequestDelegate requestDelegate,
+        IViewLocator viewLocator)
+    {
+        this.logger = logger;
+        this.requestDelegate = requestDelegate;
+        this.viewLocator = viewLocator;
+    }
+
+    /// <summary>
+    ///     Method that is invoked on the middleware.
+    /// </summary>
+    /// <param name="httpContext">The http context of the current request.</param>
+    /// <returns>A task.</returns>
+    public async Task Invoke(HttpContext httpContext)
+    {
+        try
+        {
+            await requestDelegate(httpContext);
+        }
+        catch (SidekickException ex)
+        {
+            logger.LogCritical(ex, "Unhandled exception.");
+            throw;
+        }
+    }
+}
---
+++ src/Sidekick.Linux/Program.cs
@@ -0,0 +1,95 @@
+using ApexCharts;
+using Sidekick.Apis.GitHub;
+using Sidekick.Apis.Poe;
+using Sidekick.Apis.PoeNinja;
+using Sidekick.Apis.PoePriceInfo;
+using Sidekick.Apis.PoeWiki;
+using Sidekick.Common.Updater;
+using Sidekick.Common;
+using Sidekick.Common.Blazor;
+using Sidekick.Common.Database;
+using Sidekick.Common.Platform;
+using Sidekick.Common.Platform.Interprocess;
+using Sidekick.Common.Ui;
+using Sidekick.Common.Ui.Views;
+using Sidekick.Mock;
+using Sidekick.Modules.Chat;
+using Sidekick.Modules.Development;
+using Sidekick.Modules.General;
+using Sidekick.Modules.Maps;
+using Sidekick.Modules.Trade;
+using Sidekick.Modules.Wealth;
+using Sidekick.Linux;
+using Microsoft.Extensions.FileProviders;
+using System.Reflection;
+using Velopack;
+
+var builder = WebApplication.CreateBuilder(args);
+
+#region Services
+
+builder.Services.AddRazorPages();
+builder.Services.AddServerSideBlazor();
+builder.Services.AddHttpClient();
+builder.Services.AddLocalization();
+
+builder.Services
+
+    // Common
+    .AddSidekickCommon()
+    .AddSidekickCommonBlazor()
+    .AddSidekickCommonDatabase(SidekickPaths.DatabasePath)
+    .AddSidekickCommonUi()
+    .AddSingleton<IInterprocessService, InterprocessService>()
+
+    // Apis
+    .AddSidekickGitHubApi()
+    .AddSidekickPoeApi()
+    .AddSidekickPoeNinjaApi()
+    .AddSidekickPoePriceInfoApi()
+    .AddSidekickPoeWikiApi()
+    .AddSidekickUpdater()
+
+    // Modules
+    .AddSidekickChat()
+    .AddSidekickDevelopment()
+    .AddSidekickGeneral()
+    .AddSidekickMaps()
+    .AddSidekickTrade()
+    .AddSidekickWealth()
+
+    // Mocks
+    .AddSidekickMocks();
+
+builder.Services.AddApexCharts();
+
+builder.Services.AddSingleton<IApplicationService, MockApplicationService>();
+builder.Services.AddSingleton<ITrayProvider, MockTrayProvider>();
+builder.Services.AddSingleton<IViewLocator, MockViewLocator>();
+
+#endregion Services
+
+var app = builder.Build();
+
+#region Pipeline
+
+app.Services.GetRequiredService<IInterprocessService>().StartReceiving();
+
+app.UseMiddleware<ExceptionHandlingMiddleware>();
+
+var wwwRootPath = Path.Combine(Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location)!, "wwwroot");
+app.UseStaticFiles(new StaticFileOptions
+{
+    FileProvider = new PhysicalFileProvider(wwwRootPath),
+});
+
+app.UseRouting();
+
+app.MapBlazorHub();
+app.MapFallbackToPage("/_Host");
+
+#endregion Pipeline
+
+VelopackApp.Build().Run();
+
+app.Run();
