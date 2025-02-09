6d50aadae714c79e3dc5490f540bbf52883237eb
leMicin <lemire.yanic@gmail.com>
2025-02-08 23:00:17 -0500

    Added Online in League option

--- src/Sidekick.Apis.Poe/Trade/Requests/Status.cs
+++ src/Sidekick.Apis.Poe/Trade/Requests/Status.cs
@@ -3,6 +3,7 @@ namespace Sidekick.Apis.Poe.Trade.Requests;
 public class Status
 {
     public const string Online = "online";
+    public const string OnlineLeague = "onlineleague";
     public const string Any = "any";
 
     public string Option { get; set; } = Online;
