4a43a3f8537e9e208e9ec4254f9bd4836d5a328f
leMicin <lemire.yanic@gmail.com>
2025-03-29 12:25:41 -0400

    Improved zoom handling for tooltips and popovers

---
+++ src/Sidekick.Common.Ui/App/AppWrapper.razor.js
@@ -0,0 +1,17 @@
+﻿export const initializeZoomHandling = () => {
+    let zoom = 1;
+
+    const updateVh = () => {
+        const vh = parseInt(window.innerHeight / zoom);
+        document.documentElement.style.setProperty('--vh', `${vh}px`);
+    }
+
+    window.addEventListener("resize", updateVh);
+
+    return {
+        update: (value) => {
+            zoom = value;
+            updateVh();
+        },
+    };
+};
