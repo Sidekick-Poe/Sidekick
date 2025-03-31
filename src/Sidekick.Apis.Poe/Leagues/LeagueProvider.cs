2ebda22f2cea499ec3faa6d25419ab3c19a26ce6
leMicin <lemire.yanic@gmail.com>
2025-03-30 22:51:14 -0400

    Allow initializable services to run even if the poe api is down.

--- src/Sidekick.Apis.Poe/Leagues/LeagueProvider.cs
+++ src/Sidekick.Apis.Poe/Leagues/LeagueProvider.cs
@@ -1,5 +1,6 @@
 using Microsoft.Extensions.Logging;
 using Sidekick.Apis.Poe.Clients;
+using Sidekick.Common;
 using Sidekick.Common.Cache;
 using Sidekick.Common.Enums;
 using Sidekick.Common.Exceptions;
