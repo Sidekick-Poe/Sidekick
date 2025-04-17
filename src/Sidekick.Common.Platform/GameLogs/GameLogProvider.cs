ea3e3aadf651ca72f16674729b26083014a9aef6
Dominique Alexandre <d.alexandre@outlook.com>
2025-04-17 11:18:12 -0400

    Prevent disposing BinaryReader early during GetLatestWhisper
    Reverts part of #577, should fix #643.

--- src/Sidekick.Common.Platform/GameLogs/GameLogProvider.cs
+++ src/Sidekick.Common.Platform/GameLogs/GameLogProvider.cs
@@ -87,8 +87,8 @@ public class GameLogProvider(
         var oldPosition = stream.Position;
 
         // fs.Position will be right after the '\n' char or position 0 if no '\n' char
-        using var binaryReader = new BinaryReader(stream);
-        var bytes = binaryReader.ReadBytes(lineLength - 1);
+        var bytes = new byte[lineLength - 1];
+        stream.Read(bytes, 0, lineLength - 1);
 
         // -1 is the \n
         stream.Position = oldPosition - 1;
