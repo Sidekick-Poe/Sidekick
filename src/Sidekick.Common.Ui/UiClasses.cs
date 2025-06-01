33cf3e2f22db025d7e2df8a4955076134ef261f7
leMicin <lemire.yanic@gmail.com>
2025-06-01 03:30:24 -0400

    Adjusted inputs and adornments so they are consistent with or without content.

--- src/Sidekick.Common.Ui/UiClasses.cs
+++ src/Sidekick.Common.Ui/UiClasses.cs
@@ -4,5 +4,5 @@ public static class UiClasses
 {
     public const string FocusClasses = "focus:outline-hidden focus:ring-2 focus:ring-offset-0 dark:focus:ring-violet-400";
 
-    public const string FormInputClasses = $"border rounded-lg leading-none block dark:bg-zinc-700 dark:border-zinc-600 dark:placeholder-zinc-400 dark:text-white {FocusClasses}";
+    public const string FormInputClasses = $"border rounded-lg leading-4 block dark:bg-zinc-700 dark:border-zinc-600 dark:placeholder-zinc-400 dark:text-white {FocusClasses}";
 }
