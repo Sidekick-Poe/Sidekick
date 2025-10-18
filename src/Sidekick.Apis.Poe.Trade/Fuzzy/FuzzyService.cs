c88df91a6d177b83b48766fd5268eed4a98725ab
leMicin <lemire.yanic@gmail.com>
2025-10-18 14:02:06 -0400

    Improved parsing for korean and japanese languages

--- src/Sidekick.Apis.Poe.Trade/Fuzzy/FuzzyService.cs
+++ src/Sidekick.Apis.Poe.Trade/Fuzzy/FuzzyService.cs
@@ -1,8 +1,11 @@
 using System.Text.RegularExpressions;
+using Romanization;
+using Sidekick.Apis.Poe.Languages;
+using Sidekick.Apis.Poe.Languages.Implementations;
 
 namespace Sidekick.Apis.Poe.Trade.Fuzzy;
 
-public class FuzzyService : IFuzzyService
+public class FuzzyService(IGameLanguageProvider gameLanguageProvider) : IFuzzyService
 {
     private readonly Regex cleanFuzzyPattern = new("[-+0-9%#]");
     private readonly Regex trimPattern = new(@"\s+");
@@ -16,6 +19,44 @@ public class FuzzyService : IFuzzyService
 
         text = cleanFuzzyPattern.Replace(text, string.Empty);
         text = trimPattern.Replace(text, " ").Trim();
+
+        if (gameLanguageProvider.Language is GameLanguageKo) text = RomanizeKorean(text);
+        if (gameLanguageProvider.Language is GameLanguageJa) text = RomanizeJapanese(text);
+        
+        return text;
+    }
+
+    private static Korean.RevisedRomanization? KoreanRomanization { get; set; }
+
+    private static string RomanizeKorean(string text)
+    {
+        KoreanRomanization ??= new Korean.RevisedRomanization();
+        try
+        {
+            text = KoreanRomanization.Process(text);
+        }
+        catch (Exception)
+        {
+            // Do nothing if the romanization fails.
+        }
+
+        return text;
+    }
+
+    private static Japanese.KanjiReadings? JapaneseRomanization { get; set; }
+
+    private static string RomanizeJapanese(string text)
+    {
+        JapaneseRomanization ??= new Japanese.KanjiReadings();
+        try
+        {
+            text = JapaneseRomanization.Process(text);
+        }
+        catch (Exception)
+        {
+            // Do nothing if the romanization fails.
+        }
+
         return text;
     }
 
