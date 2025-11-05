8097b84555ee2ca2b7ecacadce39cf7952a530f7
leMicin <lemire.yanic@gmail.com>
2025-11-05 13:10:47 -0500

    Included missing migration WIP

---
+++ src/Sidekick.Common.Database/Migrations/20251105174443_AddedWealthSparkline.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Sidekick.Common.Database.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddedWealthSparkline : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<decimal>(
+                name: "SparklineTotalChange",
+                table: "WealthItems",
+                type: "TEXT",
+                nullable: false,
+                defaultValue: 0m);
+
+            migrationBuilder.CreateTable(
+                name: "WealthSparklines",
+                columns: table => new
+                {
+                    ItemId = table.Column<string>(type: "TEXT", maxLength: 64, nullable: false),
+                    Index = table.Column<int>(type: "INTEGER", nullable: false),
+                    Value = table.Column<decimal>(type: "TEXT", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_WealthSparklines", x => new { x.ItemId, x.Index });
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "WealthSparklines");
+
+            migrationBuilder.DropColumn(
+                name: "SparklineTotalChange",
+                table: "WealthItems");
+        }
+    }
+}
