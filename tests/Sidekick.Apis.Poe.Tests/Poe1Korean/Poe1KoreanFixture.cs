418df9b84d5ecc9737b1fc3f364cee65df819629
leMicin <lemire.yanic@gmail.com>
2025-12-09 19:25:20 -0500

    Fixed parsing issues in Korean. Added korean unit tests.

---
+++ tests/Sidekick.Apis.Poe.Tests/Poe1Korean/Poe1KoreanFixture.cs
@@ -0,0 +1,8 @@
+using Sidekick.Apis.Poe.Items;
+namespace Sidekick.Apis.Poe.Tests.Poe1Korean;
+
+public class Poe1KoreanFixture : ParserFixture
+{
+    protected override GameType GameType => GameType.PathOfExile1;
+    protected override string Language => "ko";
+}
