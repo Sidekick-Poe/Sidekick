08f047239cfe1b11c2decfe2f1ea8cc7b3f48192
leMicin <lemire.yanic@gmail.com>
2026-04-18 15:40:54 -0400

    Various improvements to wealth tracker

--- src/Sidekick.Common.Ui/Scripts/flowbite/popover.js
+++ src/Sidekick.Common.Ui/Scripts/flowbite/popover.js
@@ -3,8 +3,9 @@ import {Popover} from 'flowbite';
 export default (triggerId, parameters) => {
     try {
         const targetId = parameters[0];
-        const placement = parameters[1] || 'top';
-        const trigger = parameters[2] || 'click';
+        const dotNetRef = parameters[1] || null;
+        const placement = parameters[2] || 'top';
+        const trigger = parameters[3] || 'click';
 
         const targetElement = document.getElementById(targetId);
         const triggerElement = document.getElementById(triggerId);
@@ -17,10 +18,23 @@ export default (triggerId, parameters) => {
             };
         }
 
+        let visible = false;
+
+        const onShow = async () => {
+            if (!visible && dotNetRef != null) dotNetRef.invokeMethodAsync('OnShow');
+            visible = true;
+        };
+
+        const onHide = async () => {
+            visible = false;
+        };
+
         console.log(`[Sidekick] Flowbite popover: initializing for target element #${targetId} and trigger element #${triggerId}`);
         const options = {
             placement: placement,
             triggerType: trigger,
+            onShow,
+            onHide,
         };
 
         return new Popover(targetElement, triggerElement, options);
