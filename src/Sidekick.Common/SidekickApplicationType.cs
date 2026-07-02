b20d2a14b285be77fcb1b38a6ed822774d1df13b
leMicin <lemire.yanic@gmail.com>
2026-07-02 15:53:29 -0400

    Replaced `LayoutSimple` with `LayoutSplash` in NotFound component, streamlined `SidekickApplicationType` by removing unused values, and removed redundant `.editorconfig` exclusion from database project.

--- src/Sidekick.Common/SidekickApplicationType.cs
+++ src/Sidekick.Common/SidekickApplicationType.cs
@@ -5,21 +5,16 @@ public enum SidekickApplicationType
     Unknown,
     Test,
     Web,
-    Photino,
-    Electron,
-    Wpf,
-    Maui,
     Avalonia,
-    DataBuilder,
 }
 
 public static class SidekickApplicationTypeExtensions
 {
-    public static bool SupportsKeybinds(this SidekickApplicationType type) => type is SidekickApplicationType.Wpf or SidekickApplicationType.Photino or SidekickApplicationType.Electron or SidekickApplicationType.Maui or SidekickApplicationType.Avalonia;
+    public static bool SupportsKeybinds(this SidekickApplicationType type) => type == SidekickApplicationType.Avalonia;
 
-    public static bool SupportsAuthentication(this SidekickApplicationType type) => type == SidekickApplicationType.Wpf || type == SidekickApplicationType.Maui || type == SidekickApplicationType.Avalonia;
+    public static bool SupportsAuthentication(this SidekickApplicationType type) => type == SidekickApplicationType.Avalonia;
 
-    public static bool SupportsDesktop(this SidekickApplicationType type) => type is SidekickApplicationType.Wpf or SidekickApplicationType.Photino or SidekickApplicationType.Electron or SidekickApplicationType.Maui or SidekickApplicationType.Avalonia;
+    public static bool SupportsDesktop(this SidekickApplicationType type) => type is SidekickApplicationType.Avalonia;
 
-    public static bool SupportsHardwareAcceleration(this SidekickApplicationType type) => type == SidekickApplicationType.Wpf || type == SidekickApplicationType.Maui || type == SidekickApplicationType.Avalonia;
+    public static bool SupportsHardwareAcceleration(this SidekickApplicationType type) => type == SidekickApplicationType.Avalonia;
 }
